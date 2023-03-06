4af1e66ab712493f0491bbcd4190386f03efcd45
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-03-06 19:08:47 +0300

    updating functions

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ document.addEventListener("DOMContentLoaded", () => {
   let birdLeft = 220;
   let birdBottom = 100;
   let gravity = 3;
+  let isGameOver = false;
+  let gap = 430;
 
   function startGame() {
     birdBottom -= gravity;
@@ -32,10 +34,51 @@ document.addEventListener("DOMContentLoaded", () => {
     let randomHeight = Math.random() * 60;
     let obstacleBottom = randomHeight;
     const obstacle = document.createElement("div");
-    obstacle.classList.add("obstacle");
+    const topObstacle = document.createElement("div");
+    if (!isGameOver) {
+      obstacle.classList.add("obstacle");
+      topObstacle.classList.add("topObstacle");
+    }
     gameDisplay.appendChild(obstacle);
+    gameDisplay.appendChild(topObstacle);
     obstacle.style.left = obstacleLeft + "px";
+    topObstacle.style.left = obstacleLeft + "px";
     obstacle.style.bottom = obstacleBottom + "px";
+    topObstacle.style.bottom = obstacleBottom + gap + "px";
+
+    function moveObstacle() {
+      obstacleLeft -= 2;
+      obstacle.style.left = obstacleLeft + "px";
+      topObstacle.style.left = obstacleLeft + "px";
+
+      if (obstacleLeft === -60) {
+        clearInterval(timerId);
+        gameDisplay.removeChild(obstacle);
+        gameDisplay.removeChild(topObstacle);
+      }
+      if (
+        (obstacleLeft > 200 &&
+          obstacleLeft < 280 &&
+          birdLeft === 220 &&
+          (birdBottom < obstacleBottom + 153 ||
+            birdBottom > obstacleBottom + gap - 200)) ||
+        birdBottom === 0
+      ) {
+        gameOver();
+        clearInterval(timerId);
+      }
+    }
+    let timerId = setInterval(moveObstacle, 20);
+    if (!isGameOver) setTimeout(generateObstacle, 3000);
   }
   generateObstacle();
+
+  function gameOver() {
+    clearInterval(gameTimerId);
+    console.log("game over");
+    isGameOver = true;
+    document.removeEventListener("keyup", control);
+    ground.classList.add("ground");
+    ground.classList.remove("ground-moving");
+  }
 });

a53b751d8a5224b72be859cf1cbbfc037bab75a9
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-03-06 16:32:03 +0300

    adding obstacle

--- app.js
+++ app.js
@@ -14,10 +14,28 @@ document.addEventListener("DOMContentLoaded", () => {
   }
   let gameTimerId = setInterval(startGame, 20);
 
+  function control(e) {
+    if (e.keyCode === 32) {
+      jump();
+    }
+  }
+
   function jump() {
     if (birdBottom < 500) birdBottom += 50;
     bird.style.bottom = birdBottom + "px";
     console.log(birdBottom);
   }
   document.addEventListener("keyup", control);
+
+  function generateObstacle() {
+    let obstacleLeft = 500;
+    let randomHeight = Math.random() * 60;
+    let obstacleBottom = randomHeight;
+    const obstacle = document.createElement("div");
+    obstacle.classList.add("obstacle");
+    gameDisplay.appendChild(obstacle);
+    obstacle.style.left = obstacleLeft + "px";
+    obstacle.style.bottom = obstacleBottom + "px";
+  }
+  generateObstacle();
 });
